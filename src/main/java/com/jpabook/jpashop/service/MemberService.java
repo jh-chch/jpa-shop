cf30f61c4df01c36c5c06f184af8eaf3832f3047
jhchoi <jhchoig9@gamil.com>
2024-10-07 11:55:36 +0900

    회원 도메인 개발
    - 회원 레로지토리
    - 회원 서비스
    - 기능 테스트

---
+++ src/main/java/com/jpabook/jpashop/service/MemberService.java
@@ -0,0 +1,41 @@
+package com.jpabook.jpashop.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.jpabook.jpashop.domain.Member;
+import com.jpabook.jpashop.repository.MemberRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    @Transactional
+    public Long join(Member member) {
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        List<Member> findMembers = memberRepository.findByName(member.getName());
+        if (!findMembers.isEmpty()) {
+            throw new IllegalStateException("존재하는 회원");
+        }
+    }
+
+    public List<Member> findMemebers() {
+        return memberRepository.findAll();
+    }
+
+    public Member findOne(Long id) {
+        return memberRepository.findOne(id);
+    }
+}
---
+++ src/test/java/com/jpabook/jpashop/service/MemberServiceTest.java
@@ -0,0 +1,58 @@
+package com.jpabook.jpashop.service;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.jpabook.jpashop.domain.Member;
+import com.jpabook.jpashop.repository.MemberRepository;
+
+import jakarta.persistence.EntityManager;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Transactional
+public class MemberServiceTest {
+
+    @Autowired
+    MemberService memberService;
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Autowired
+    EntityManager em;
+
+    @Test
+    @Rollback(false)
+    public void 회원가입() throws Exception {
+        Member member = new Member();
+        member.setName("choi");
+
+        Long saveId = memberService.join(member);
+
+        assertEquals(member, memberRepository.findOne(saveId));
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void 중복_회원_예외() throws Exception {
+        Member member1 = new Member();
+        member1.setName("choi");
+
+        Member member2 = new Member();
+        member2.setName("choi");
+
+        memberService.join(member1);
+        memberService.join(member2);
+
+        fail("예외가 발생해야 한다.");
+    }
+}
